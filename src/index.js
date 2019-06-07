a36faba268092f980f5de7c587e3448c2ca5f06e
Miguel Rojas <miguel_rojas@mentor.com>
2019-06-07 16:31:17 -0400

    Adding the static and dynamic test components

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import ReactDOM from "react-dom";
 
 // We import out components
 import App from "./App.js";
-//import StaticTestComponent from "./Components/StaticTestComponent.js";
-//import DynamicTestComponent from "./Components/DynamicTestComponent.js";
+import StaticTestComponent from "./Components/StaticTestComponent.js";
+import DynamicTestComponent from "./Components/DynamicTestComponent.js";
 import NoteListComponent from "./Components/NoteList";
 
 // Render the components
 ReactDOM.render(<App />, document.getElementById("root"));
 //ReactDOM.render([<StaticTestComponent title="Static content with properties" />], document.getElementById("rootContent"));
 //ReactDOM.render([<DynamicTestComponent />, <StaticTestComponent title="Static content with properties" />], document.getElementById("rootContent"));
-ReactDOM.render([<NoteListComponent title="List of my notes" />], document.getElementById("rootContent"));
+ReactDOM.render([<NoteListComponent title="List of my notes" />, <StaticTestComponent title="Static content with properties" />, <DynamicTestComponent />], document.getElementById("rootContent"));
